41a133c9203acdaedab8e4973ec56a67557eb79f
shyheng <shy2210278285@aliyun.com>
2021-02-03 21:57:23 +0800

    测试

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //打印
+            Console.WriteLine("Hello World!");
+            //阻塞
+            Console.ReadKey();
+            // 按下任意键继续执行
+            Console.WriteLine("shy");
+        }
+    }
+}
