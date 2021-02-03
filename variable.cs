09f6a0d798b77844bb056c4903963ef6435dcd18
shyheng <shy2210278285@aliyun.com>
2021-02-03 22:14:08 +0800

    变量

---
+++ variable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace variable
+{
+    class variable
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 1.2;
+            string s = "shy";
+            char s1 = 's';
+            decimal c = 1.25534m;
+
+            /*String ss = "shy";*/
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(s + a + b);
+            Console.WriteLine(s1);
+            Console.WriteLine(c);
+        }
+    }
+}
