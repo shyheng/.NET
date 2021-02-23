818dbe11a49205b63ecadd3150b5b75913d167e2
shyheng <shy2210278285@aliyun.com>
2021-02-24 01:09:18 +0800

    转义字符

---
+++ escape_character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace escape_character
+{
+    class escape_character
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("转义\" \t,\n sss\b");
+            /*@按照原格式输出*/
+            Console.WriteLine(@"shy
+                heng");
+        }
+    }
+}
