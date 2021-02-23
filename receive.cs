92e97267f166f9856a37cb686a2259b09ac54db1
shyheng <shy2210278285@aliyun.com>
2021-02-24 00:22:19 +0800

    接收用户输出

---
+++ receive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace receive
+{
+    class receive
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输出");
+            string std = Console.ReadLine();
+            Console.WriteLine("名字{0}",std);
+            Console.ReadKey();
+        }
+    }
+}
