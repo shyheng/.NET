7b1d4ecee01ab3eb4c7e0576c47808a263a39bd9
shyheng <shy2210278285@aliyun.com>
2021-02-23 23:40:56 +0800

    字符串的传递

---
+++ variable1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace variable1
+{
+    class variable1
+    {
+        static void Main(string[] args)
+        {
+            string name = "shy";
+            Console.WriteLine("Hello"+name);
+        }
+    }
+}
