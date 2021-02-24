6bbfbac7726b670417e9a8e5bf91916caf5a377e
shyheng <shy2210278285@aliyun.com>
2021-02-24 23:10:12 +0800

    类型转换-类型兼容

---
+++ type_conversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace type_conversion
+{
+    class type_conversion
+    {
+        static void Main(string[] args)
+        {
+            /*            int m = 10;
+                        double d = m;*///自动类型转换 隐式
+
+            double m = 21.15;
+            int i = (int)m;//强制类型转换 显性
+
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
