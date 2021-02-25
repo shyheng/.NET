da8d211c5b58cd5aa4e6bac830964cb8d17d6ba3
shyheng <shy2210278285@aliyun.com>
2021-02-25 20:24:21 +0800

    类型转换-不兼容类型

---
+++ type_conversion1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace type_conversion1
+{
+    class type_conversion1
+    {
+        static void Main(string[] args)
+        {
+            //类型转换
+            //不兼容
+            string s = "123";
+
+            double d = Convert.ToDouble(s);
+
+            int i = Convert.ToInt32(s);
+
+            Console.WriteLine(d);
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
